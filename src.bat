a8f570a83a3b0809e8168fd5c542f1debdb5b9ae
CosmologicalButter <53821637+Normolo@users.noreply.github.com>
2023-02-15 14:29:51 +0100

    Update src.bat

--- src.bat
+++ src.bat
@@ -1,6 +1,20 @@
 @echo off
+setlocal EnableExtensions EnableDelayedExpansion
+
+set target_file=dir_tree.txt
+
+echo Checking if file already exists...
+if exist "%target_file%" (
+    set /p overwrite=The file already exists. Overwrite it? (Y/N)
+    if /i "!overwrite!" EQU "N" (
+        echo Exiting script...
+        goto :EOF
+    )
+)
+
 echo Generating directory tree...
-tree /a /f > dir_tree.txt
-echo Done! Opening dir_tree.txt...
-start notepad dir_tree.txt
-pause>nul
+tree /a /f > "%target_file%"
+echo Done! Opening %target_file%...
+start "" notepad "%target_file%"
+
+pause >nul
